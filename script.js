f75512d50b795c5627e8b21b46b7eab82a4bd5c6
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-03 12:28:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let buttons = document.querySelectorAll(".buttons")
+let currentSound;
+
+buttons.forEach(() => {
+	  bttn.addEventListener("click", () => {
+		  const sound = bttn.getAttribute("data-sound")
+		  playSound(sound)
+	  })
+})
+
+let stop = document.querySelector(".stop")
+
+stop.addEventListener("click", stop())
+
+function playSound(sound) {
+	 stop()
+	currentSound = new Audio(`${sound}.mp3`)
+	currentSound.play()
+}
+
+function stop(sound){
+   if(currentSound){
+    currentSound.pause()
+	currentSound.currentTime = 0
+   }
+}
